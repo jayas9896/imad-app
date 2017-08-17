bd7a3ad62fe64755f9baab43e90c5f8588ccbb44
Padishala Jayashanker <jayas9896@gmail.com>
2017-08-17 17:30:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.get('/article-one', function(req,res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
+app.get('/article-two', function(req,res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+
+app.get('/article-three', function(req,res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
+
 app.get('/article-three', function(req,res) {
     res.send('Article three requested and will be served here');
 });
